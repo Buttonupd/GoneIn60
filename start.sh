a2c745853569b8ebc492a1884602ecf963d2e717
Buttonupd <Dankariuki0101@gmail.com>
2020-05-05 10:22:02 +0300

    Adjusted files to handle ap in production

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 SECRET_KEY='THE DAY COMMETH'
 export MAIL_USERNAME='Dankariuki0101@gmail.com'
 export MAIL_PASSWORD='Patterns2323@q'
+export PYTHONPATH=$PYTHON:/usr/lib/python3.7.5
+
 python3.7 manage.py server
